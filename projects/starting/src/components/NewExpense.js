37a29c63e55f28b2576644d0623f7b87b30d75aa
Ronald Tagolino <tags0516@gmail.com>
2023-08-22 23:43:25 +0800

    Update for using state.

--- projects/starting/src/components/NewExpense.js
+++ projects/starting/src/components/NewExpense.js
@@ -2,10 +2,18 @@ import ExpenseForm from "./ExpenseForm"
 
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const ExpenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(ExpenseData);
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
     );
 };
