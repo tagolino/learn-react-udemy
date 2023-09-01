f67225f49f7719ab617e0cb27c42bdfd3ed848c8
Ronald Tagolino <tags0516@gmail.com>
2023-09-01 23:00:46 +0800

    Add expense chart.

---
+++ projects/starting/src/components/Chart.js
@@ -0,0 +1,19 @@
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = props => {
+    const totalMaxValue = Math.max(...props.dataPoints.map(dataPoint => dataPoint.value));
+
+    return <div className="chart">
+        {props.dataPoints.map((dataPoint) => (
+            <ChartBar
+                key={dataPoint.label}
+                value={dataPoint.value}
+                maxValue={totalMaxValue}
+                label={dataPoint.label}
+            />
+        ))}
+    </div>
+};
+
+export default Chart;
